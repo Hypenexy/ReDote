3608bdcad3b7a74074c5a4a814b43cb990676dfa
Midelight <61563710+Hypenexy@users.noreply.github.com>
2021-09-25 01:13:39 +0300

    what the

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const processesbtn = document.getElementById("processesbtn");
+const processes = document.getElementById("processes");
+const networkbtn = document.getElementById("networkbtn");
+const network = document.getElementById("network");
 
-function open(){
-    processesbtn.style.filter = "none";
+const panels = ["processes", "network"];
+
+okey.onclick = open("processes");
+okey.onclick = open("network");
+
+function open(n){
+  panels.forEach(hideactiveloop);
+  document.getElementById(n + "btn").style.filter = "none";
+  document.getElementById(n).style.display = "block";
 }
 
-open();
+function hideactiveloop(e) {
+  if(document.getElementById(e).style.display == "block"){
+    document.getElementById(e).style.removeProperty("display");
+    document.getElementById(e + "btn").style.removeProperty("Filter");
+  }
+}
+
+
+//open("processes");
